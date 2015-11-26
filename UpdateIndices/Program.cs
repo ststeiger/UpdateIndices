b000f5e04cd16c506f46fe0b0ffe7b93f1f118fd
ststeiger <quandary82@hailmail.net>
2015-11-26 17:35:53 +0100

    Initial commit

---
+++ UpdateIndices/Program.cs
@@ -0,0 +1,79 @@
+﻿
+namespace UpdateIndices
+{
+
+
+    static class Program
+    {
+
+
+        public static string MapVisualStudioPath(string fileName)
+        {
+            string dir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            dir = System.IO.Path.Combine(dir, "../..");
+            dir = System.IO.Path.GetFullPath(dir);
+            dir = System.IO.Path.Combine(dir, fileName);
+
+            return dir;
+        }
+
+
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [System.STAThread]
+        static void Main()
+        {
+
+            if (false)
+            {
+                // Suspend the screen.
+                System.Windows.Forms.Application.EnableVisualStyles();
+                System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+                System.Windows.Forms.Application.Run(new Form1());
+            }
+
+
+            int counter = 0;
+            string line;
+
+            using (System.IO.StreamReader file = new System.IO.StreamReader(MapVisualStudioPath("AllMissingIndices.sql")))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line == null)
+                        continue;
+
+                    line = line.Trim();
+
+                    if (line == string.Empty)
+                        continue;
+
+                    if(System.StringComparer.InvariantCultureIgnoreCase.Equals(line, "GO"))
+                        continue;
+
+
+                    System.Console.WriteLine(line);
+                    int retVal = SQL.ExecuteNonQuery(line);
+                    if(retVal == -1)
+                        System.Console.WriteLine("Failed");
+                    else
+                        System.Console.WriteLine("Succeeded");
+
+                    System.Console.WriteLine(System.Environment.NewLine);
+
+                    counter++;
+                }
+
+                file.Close();
+            }
+            
+            System.Console.WriteLine(" === Press any key to continue === ");
+            System.Console.ReadKey();
+        }
+
+
+    }
+
+
+}
---
+++ UpdateIndices/SQL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateIndices
+{
+    class SQL
+    {
+
+        public static string GetConnectionString()
+        {
+            System.Data.SqlClient.SqlConnectionStringBuilder csb = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            csb.DataSource = System.Environment.MachineName;
+            csb.InitialCatalog = "RoomPlanning";
+            
+            csb.IntegratedSecurity = true;
+
+            return csb.ConnectionString;
+        }
+
+
+        public static int ExecuteNonQuery(string strSQL)
+        { 
+            int retVal = 0;
+
+            try
+            {
+
+                using (System.Data.Common.DbConnection con = new System.Data.SqlClient.SqlConnection(GetConnectionString()))
+                {
+                    using (System.Data.Common.DbCommand cmd = con.CreateCommand())
+                    {
+                        cmd.CommandText = strSQL;
+
+                        if (cmd.Connection.State != System.Data.ConnectionState.Open)
+                            cmd.Connection.Open();
+
+                        retVal = cmd.ExecuteNonQuery();
+
+                        if (cmd.Connection.State != System.Data.ConnectionState.Closed)
+                            cmd.Connection.Close();
+                    } // End Using cmd 
+                }
+            }
+            catch (System.Exception ex)
+            {
+                // System.Console.WriteLine(ex.Message);
+                retVal = -1;
+            }
+
+            return retVal;
+        }
+
+
+
+    }
+}
